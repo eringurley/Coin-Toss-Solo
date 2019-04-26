3007146a7968a2ea9817b133132f6ab70cbe95c1
Erin Gurley <erinmgurley@gmail.com>
2019-04-26 10:39:03 -0700

    added tests folder and function in tests.js, also added const winsCount and const lossesCount in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
-import pandaFlip from (.src/panda-pandaFlip.js)
+import pandaFlip from './panda-flip.js';
+const submitFlip = document.getElementById('submit');
+const pandaSide = document.getElementById('panda');
+const messageSays = document.getElementById('message');
+const headsGuess = document.getElementById('heads');
+const winsCount = document.getElementById('wins');
+const lossesCount = document.getElementById('losses');
+
+// defining scoring variables
+let wins = 0;
+let losses = 0;
+
+//flip or submit the panda flip
+submitFlip.addEventListener('click', () => {
+    const randomNum = Math.random();
+    
+//change image display
+    const flip = pandaFlip(randomNum);
+    const image = 'assets/' + flip + '.jpg';
+    pandaSide.src = image;
+    
+//did the user guess correctly?
+    let userGuess = '';
+    if(headsGuess.checked) {
+        userGuess = 'heads';
+    }
+    else { 
+        userGuess = 'tails';
+    }
+
+//show message
+    if(flip === userGuess) {
+        messageSays.textContent = 'Ye!';
+        wins++;
+        winsCount.textContent = 'Wins: ' + wins; 
+        messageSays.classList.remove('loss');
+        messageSays.classList.add('win');
+
+    }
+    else {
+        messageSays.textContent = 'Awwww!';
+        losses++;
+        lossesCount.textContent = 'Losses: ' + losses; 
+        messageSays.classList.remove('win');
+        messageSays.classList.add('loss');
+
+    }
+});
+
